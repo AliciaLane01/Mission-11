8dd02c8118f6ad85e57264b55e65babf7ae32492
AliciaLane01 <62069491+AliciaLane01@users.noreply.github.com>
2022-03-16 22:51:08 -0600

    Main Project

---
+++ BookStore/BookStore/Models/IBookStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public interface IBookStoreRepository
+    {
+        IQueryable<Book> Books { get; }
+
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
+
+    }
+}
